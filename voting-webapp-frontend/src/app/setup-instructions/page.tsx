167e71812e53f4136143ec1fb7dce5867dfdbab6
J Cui <i@cjxol.com>
2025-01-26 01:10:44 +0000

    Add link to github

--- voting-webapp-frontend/src/app/setup-instructions/page.tsx
+++ voting-webapp-frontend/src/app/setup-instructions/page.tsx
@@ -11,9 +11,14 @@ export default function SetupInstructions() {
         Please ensure form setup correctly before sharing. Inconsistent response data may break the app and lead to unexpected results. Users might already have the old form loaded, and they may still be able to submit the data for before the changes to the updated form
       </Alert>
       <Alert variant="light" color="yellow" title="Do NOT use semicolon &quot;;&quot; in ranking candidate names" icon={<IconAlertTriangle />} mt='md'>
-        Using semicolon in candidate names will break the app and lead to unexpected results. If all non-empty responses to a question ends with a semicolon, it may be guessed as a ranking question 
+        Using semicolon in candidate names will break the app and lead to unexpected results. If all non-empty responses to a question ends with a semicolon, it may be guessed as a ranking question
       </Alert>
-      <Anchor href='https://docs.google.com/document/d/1kigaB21ccfpu9L-0dmraBEwQgRBVMbvdGfhnug17d_M/edit?usp=sharing' target='_blank'>Read instructions</Anchor>
+      <div>
+        <Anchor href='https://docs.google.com/document/d/1kigaB21ccfpu9L-0dmraBEwQgRBVMbvdGfhnug17d_M/edit?usp=sharing' target='_blank'>Read instructions</Anchor>
+      </div>
+      <div>
+        <Anchor href='https://github.com/allc/voting-webui' target='_blank'>View source code</Anchor>
+      </div>
     </>
   )
 }
